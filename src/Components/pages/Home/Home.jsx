ec4de66111660f6ca5fa999956feb63aedfd9666
muhammadhf <muhammadhf91@gmail.com>
2025-05-17 14:05:09 +0700

    test

--- src/Components/pages/Home/Home.jsx
+++ src/Components/pages/Home/Home.jsx
@@ -8,7 +8,7 @@ import Projects from "../Projects/Project";
 
 const fadeInUp = {
   hidden: { opacity: 0, y: 30 },
-  visible: { opacity: 1, y: 0, transition: { duration: 0.8, ease: "easeOut" } },
+  visible: { opacity: 1, y: 0, transition: { duration: 0.7, ease: "easeOut" } },
 };
 
 const stagger = {
